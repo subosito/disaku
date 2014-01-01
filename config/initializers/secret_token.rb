cd803ca386902681385b51b549b41e4c3e90e44e
Alif Rachmawadi <subosito@gmail.com>
2014-01-01 22:30:48 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Disaku::Application.config.secret_key_base = 'd701c6d22bf2ab6638a1199caefb20decdd88999152fefd03d125562169ae0594d3e2a54929d6f8711841879701e3727eda30f3fbf0450fd5f40c407f3ed16f5'
