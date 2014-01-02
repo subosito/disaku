025ebc09a6dbc612bd42c059a67136446d250779
Alif Rachmawadi <subosito@gmail.com>
2014-01-02 22:14:14 +0700

    add basic appearance; implements decorator for accountable

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -1,3 +1,23 @@
 class AccountDecorator < ApplicationDecorator
   delegate_all
+
+  def total_expense
+    h.to_currency(account.total_expense)
+  end
+
+  def monthly_expense
+    h.to_currency(account.monthly_expense)
+  end
+
+  def total_income
+    h.to_currency(account.total_income)
+  end
+
+  def monthly_income
+    h.to_currency(account.monthly_income)
+  end
+
+  def balance
+    h.to_currency(account.balance)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def to_currency(amount)
+    number_to_currency(amount, unit: 'Rp', precision: 0, format: "%u %n", separator: ',', delimiter: '.')
+  end
 end
