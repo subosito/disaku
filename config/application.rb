126139d4bd6708e82960f648acb3f71b80f92718
Alif Rachmawadi <subosito@gmail.com>
2014-01-03 16:39:27 +0700

    fix missing fonts

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Disaku
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Assets Precompilation
+    config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
   end
 end
