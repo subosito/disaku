b95395c29726084a3eaa9c610bfbac0fa698246c
Alif Rachmawadi <subosito@gmail.com>
2014-09-15 21:16:10 +0700

    enable smtp mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,17 @@ Disaku::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_DOMAIN'] }
+
+  # SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name      => ENV['MAILER_USERNAME'],
+    :password       => ENV['MAILER_PASSWORD'],
+    :address        => ENV['MAILER_ADDRESS'],
+    :port           => ENV['MAILER_PORT'],
+    :authentication => :plain
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ Disaku::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_DOMAIN'] }
+
+  # SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name      => ENV['MAILER_USERNAME'],
+    :password       => ENV['MAILER_PASSWORD'],
+    :address        => ENV['MAILER_ADDRESS'],
+    :port           => ENV['MAILER_PORT'],
+    :authentication => :plain
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
