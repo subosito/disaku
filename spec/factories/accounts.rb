f62fd3b7a7626821f84fe300833b3c4bd855f107
Alif Rachmawadi <subosito@gmail.com>
2014-09-17 06:58:03 +0700

    added test for date_range

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -2,5 +2,17 @@ FactoryGirl.define do
   factory :account do
     sequence(:name) { |n| "Account ##{n}" }
     initial_amount  { 10 }
+
+    trait :cash do
+      account_type { :cash }
+    end
+
+    trait :saving do
+      account_type { :saving }
+    end
+
+    trait :other do
+      account_type { :other }
+    end
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe Transaction, :type => :model do
     it { is_expected.to delegate_method(:category_name).to(:category).as(:name) }
     it { is_expected.to delegate_method(:category_type).to(:category) }
   end
+
+  context "scopes" do
+    let!(:transaction_a) { create(:transaction, transaction_date: Date.today) }
+    let!(:transaction_b) { create(:transaction, transaction_date: 1.day.ago) }
+    let!(:transaction_c) { create(:transaction, transaction_date: 2.days.ago) }
+    let!(:transaction_d) { create(:transaction, transaction_date: 3.days.ago) }
+
+    describe ".date_range" do
+      it { expect(described_class.date_range(1.day.ago, Date.today)).to match_array([transaction_a, transaction_b]) }
+      it { expect(described_class.date_range(3.day.ago, 1.day.ago)).to  match_array([transaction_b, transaction_c, transaction_d]) }
+    end
+  end
 end
