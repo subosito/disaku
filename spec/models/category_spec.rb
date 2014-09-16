7ed23b7e58c7efacc8e526141949cf057cda2189
Alif Rachmawadi <subosito@gmail.com>
2014-09-16 07:49:07 +0700

    added tests for grouped_select

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Category, :type => :model do
   context "validations" do
     it { is_expected.to enumerize(:category_type).in(:income, :expense, :credit) }
   end
+
+  describe ".grouped_options_for_select" do
+    let!(:category_a) { create(:category, :expense) }
+    let!(:category_b) { create(:category, :income) }
+    let!(:category_c) { create(:category, :credit) }
+
+    it { expect(described_class.grouped_options_for_select.length).to eq(3) }
+    it { expect(described_class.grouped_options_for_select).to include({ "Expense" => [category_a] }) }
+    it { expect(described_class.grouped_options_for_select).to include({ "Income"  => [category_b] }) }
+    it { expect(described_class.grouped_options_for_select).to include({ "Credit"  => [category_c] }) }
+  end
 end
