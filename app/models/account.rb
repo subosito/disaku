4b5cbb67be8ab3a856bbe11801c4afd8e6a62b21
Alif Rachmawadi <subosito@gmail.com>
2014-09-16 07:11:19 +0700

    scoped account name uniqueness by user

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,7 +8,7 @@ class Account < ActiveRecord::Base
   has_many :sent_transfers,     class_name: 'Transfer', foreign_key: 'from_account_id'
   has_many :received_transfers, class_name: 'Transfer', foreign_key: 'to_account_id'
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :initial_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   enumerize :account_type, in: [:saving, :cash, :other]
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Account, :type => :model do
-  it { is_expected.to belong_to(:user) }
-  it { is_expected.to have_many(:transactions) }
-  it { is_expected.to have_many(:sent_transfers).class_name('Transfer').with_foreign_key('from_account_id') }
-  it { is_expected.to have_many(:received_transfers).class_name('Transfer').with_foreign_key('to_account_id') }
+  context "associations" do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:transactions) }
+    it { is_expected.to have_many(:sent_transfers).class_name('Transfer').with_foreign_key('from_account_id') }
+    it { is_expected.to have_many(:received_transfers).class_name('Transfer').with_foreign_key('to_account_id') }
+  end
+
+  context "validations" do
+    it { is_expected.to enumerize(:account_type).in(:saving, :cash, :other) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:user_id) }
+    it { is_expected.to validate_presence_of(:initial_amount) }
+    it { is_expected.to validate_numericality_of(:initial_amount).is_greater_than_or_equal_to(0) }
+  end
 end
